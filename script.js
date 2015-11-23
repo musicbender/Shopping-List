669b17afde5ec1f01a9912855312e9ddfa52ecec
musicbender <musicbender@gmail.com>
2015-11-22 19:26:56 -0800

    added blank div and containing shopping list div

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
     
     $('.input-control').submit(function(){
-        //var formVal = $('.list-form').val();
-        console.log("goats ");
-        alert('goats');
+        var formVal = $('.list-form').val();
+        
+        
     });
+    
+    
+    
+    
+    
+    
+    //Show delete button when hovering
+    $('.item-text').mouseenter(function(){
+        $(this).children().show();
+    });
+    
+    $('.item-text').mouseleave(function(){
+        $(this).children().hide();
+    });
+    
 });
+
