6e0a370dc075529f6379d2f14960402731b352c4
musicbender <musicbender@gmail.com>
2015-11-23 19:08:02 -0800

    polish

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ $(document).ready(function(){
     $('.input-control').submit(function(){
         var formVal = $('.list-form').val();
         
-        $('.shopping-list').first().prepend('<div class="list-item"><div class="check-box"><div class="unchecked"></div></div><div class="item-text">' + formVal + '<div class="delete">X</div></div></div>');
+        $('.shopping-list').first().prepend('<div class="list-item"><div class="check-box"><div class="unchecked"></div></div><div class="item-text">' + formVal + '<div class="delete">X</div></div></div>').show('slow');
+        $('.list-item').slideDown(500, 'swing');
         
         $('.list-form').val('');
         
@@ -28,7 +29,7 @@ $(document).ready(function(){
     
     //Hover over checkbox
     $('.shopping-list').on('mouseenter', '.check-box, .check-box-checked', function(){
-        $(this).children().css('background-color', '#FF822D');
+        $(this).children().css('background-color', 'white');
     });
     
     $('.shopping-list').on('mouseleave', '.check-box', function(){
@@ -43,7 +44,7 @@ $(document).ready(function(){
     //Check off list item 
     $('.shopping-list').on('click', '.check-box', function(){
         $(this).removeClass('check-box').addClass('check-box-checked');
-        $(this).children().css('background-color', '#FF822D');
+        $(this).children().css('background-color', 'white');
         
         $(this).children().removeClass('unchecked').addClass('checked');
         $(this).siblings().addClass('text-checked');
@@ -53,7 +54,7 @@ $(document).ready(function(){
     //Re-uncheck list item
     $('.shopping-list').on('click', '.check-box-checked', function(){
         $(this).removeClass('check-box-checked').addClass('check-box');
-        $(this).children().css('background-color', '#FF822D');
+        $(this).children().css('background-color', 'white');
         
         $(this).children().removeClass('checked').addClass('unchecked');
         $(this).siblings().removeClass('text-checked');
