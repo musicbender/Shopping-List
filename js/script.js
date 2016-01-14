eb9138c4d93ba3d2572863279c01244765fc9019
musicbender <musicbender@gmail.com>
2016-01-14 15:19:30 -0800

    made folders

--- js/script.js
+++ js/script.js
@@ -3,40 +3,33 @@ $(document).ready(function(){
     //Puts focus on form when page loads
     $('.list-form').focus();
     
-    
     //Add new list item
     $('.input-control').submit(function(){
         var formVal = $('.list-form').val();
         
         $('.shopping-list').first().prepend('<div class="list-item"><div class="check-box"><div class="unchecked"></div></div><div class="item-text">' + formVal + '<div class="delete">X</div></div></div>').show('slow');
         $('.list-item').slideDown(500, 'swing');
-        
         $('.list-form').val('');
         
         return false;
     });
     
-    
     //Show delete button when hovering
     $('.shopping-list').on('mouseenter', '.item-text', function(){
         $(this).children().show();
     });
-    
     $('.shopping-list').on('mouseleave', '.item-text', function(){
         $(this).children().hide();
     });
-    
     
     //Hover over checkbox
     $('.shopping-list').on('mouseenter', '.check-box, .check-box-checked', function(){
         $(this).children().css('background-color', 'white');
     });
-    
     $('.shopping-list').on('mouseleave', '.check-box', function(){
         $(this).children().css('background-color', 'black');
     });
-    
-        $('.shopping-list').on('mouseleave', '.check-box-checked', function(){
+    $('.shopping-list').on('mouseleave', '.check-box-checked', function(){
         $(this).children().css('background-color', 'transparent');
     });
     
@@ -45,17 +38,14 @@ $(document).ready(function(){
     $('.shopping-list').on('click', '.check-box', function(){
         $(this).removeClass('check-box').addClass('check-box-checked');
         $(this).children().css('background-color', 'white');
-        
         $(this).children().removeClass('unchecked').addClass('checked');
         $(this).siblings().addClass('text-checked');
     });
-    
     
     //Re-uncheck list item
     $('.shopping-list').on('click', '.check-box-checked', function(){
         $(this).removeClass('check-box-checked').addClass('check-box');
         $(this).children().css('background-color', 'white');
-        
         $(this).children().removeClass('checked').addClass('unchecked');
         $(this).siblings().removeClass('text-checked');
     });
